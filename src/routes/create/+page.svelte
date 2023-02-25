7043acff3d48532bad0788d2a41ce977a2869fa3
nbur4556 <nbur4556@gmail.com>
2023-02-24 19:46:26 -0600

    create a component for selecting the pets goal type

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -4,11 +4,16 @@
   import { pet } from '../../stores'
 	import type { PetType } from '../../types/PetType';
 
+  import SelectPetType from "./SelectPetType.svelte";
+
+  let step: number = 0;
 	let formValues: PetType = {
 		name: '',
 		description: '',
 		type: ''
 	};
+
+  //TODO: on select type sets the pet value and navigates to step 1
 
 	const onSubmit = () => {
     pet.update(() => formValues);
@@ -17,29 +22,33 @@
 </script>
 
 <main>
-	<h1>Create Your Goal Pet</h1>
+  {#if step === 0}
+    <SelectPetType />
+  {:else if step === 1}
+    <form on:submit|preventDefault={onSubmit}>
+      <label for="name">Name</label>
+      <input type="text" name="name" bind:value={formValues.name} />
 
-	<form on:submit|preventDefault={onSubmit}>
-		<label for="name">Name</label>
-		<input type="text" name="name" bind:value={formValues.name} />
+      <label for="description">Description</label>
+      <input type="text" name="description" bind:value={formValues.description} />
 
-		<label for="description">Description</label>
-		<input type="text" name="description" bind:value={formValues.description} />
+      <!-- //TODO: Remove goal type from form and use value from select pet type steps -->
+      <label for="type">Goal Type</label>
+      <select name="type" bind:value={formValues.type}>
+        <option value="" />
+        <option value="intellectual">Intellectual</option>
+        <option value="physical">Physical</option>
+        <option value="social">Social</option>
+        <option value="financial">Financial</option>
+        <option value="occupational">Occupational</option>
+        <option value="spiritual">Spiritual</option>
+        <option value="environmental">Environmental</option>
+        <option value="emotional">Emotional</option>
+        <option value="other">Other</option>
+      </select>
 
-		<label for="type">Goal Type</label>
-		<select name="type" bind:value={formValues.type}>
-			<option value="" />
-			<option value="intellectual">Intellectual</option>
-			<option value="physical">Physical</option>
-			<option value="social">Social</option>
-			<option value="financial">Financial</option>
-			<option value="occupational">Occupational</option>
-			<option value="spiritual">Spiritual</option>
-			<option value="environmental">Environmental</option>
-			<option value="emotional">Emotional</option>
-			<option value="other">Other</option>
-		</select>
-
-		<button type="submit">Submit</button>
-	</form>
+      <button type="submit">Submit</button>
+    </form>
+  {/if}
+	
 </main>
---
+++ src/routes/create/SelectPetType.svelte
@@ -0,0 +1,19 @@
+<!-- //TODO: handle on click selection from a prop function -->
+<h1>New Creature</h1>
+
+<p>Your creature is a representation of a goal you want to achieve</p>
+
+<h2>Choose a goal type:</h2>
+
+<ul>
+  <li>Intellectual</li>
+  <li>Physical</li>
+  <li>Social</li>
+  <li>Financial</li>
+  <li>Occupational</li>
+  <li>Spiritual</li>
+  <li>Environmental</li>
+  <li>Emotional</li>
+  <!-- //TODO: if other is seleted, provide form to name that goal type -->
+  <li>Other</li>
+</ul>
