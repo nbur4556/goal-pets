54d3f5be6a75cd312f8a8332b6b78b0ff029eb59
nbur4556 <nbur4556@gmail.com>
2023-02-26 14:59:01 -0600

    create initial test creature as an svg component

---
+++ src/components/creatures/TempCreature.svelte
@@ -0,0 +1,32 @@
+<svg
+	width="100%"
+	height="100%"
+	viewBox="0 0 9934 14044"
+	version="1.1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	xml:space="preserve"
+	style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"
+>
+	<g transform="matrix(1,0,0,1,417.035,699.5)">
+		<circle cx="4549.5" cy="6916.5" r="2961.5" style="fill:rgb(114,146,128);" />
+	</g>
+	<g transform="matrix(1,0,0,1,146,-256)">
+		<path
+			d="M3987,5090C3987,5090 3283.08,4113.94 3035,3570C2733,2907.83 2252.67,423.75 1972,568C1691.33,712.25 1324.22,3380.64 1351,4435.5C1378.5,5518.67 2340,7616 2340,7616L3987,5090Z"
+			style="fill:rgb(114,146,128);"
+		/>
+	</g>
+	<g transform="matrix(-1,0,0,1,9814.54,-256)">
+		<path
+			d="M3987,5090C3987,5090 3283.08,4113.94 3035,3570C2733,2907.83 2252.67,423.75 1972,568C1691.33,712.25 1324.22,3380.64 1351,4435.5C1378.5,5518.67 2340,7616 2340,7616L3987,5090Z"
+			style="fill:rgb(114,146,128);"
+		/>
+	</g>
+	<g transform="matrix(1.95513,0,0,1.95513,-3428.46,-6282.67)">
+		<circle cx="3867.5" cy="6843.33" r="265.5" style="fill:rgb(222,250,235);" />
+	</g>
+	<g transform="matrix(1.95513,0,0,1.95513,-1733.92,-6282.67)">
+		<circle cx="3867.5" cy="6843.33" r="265.5" style="fill:rgb(222,250,235);" />
+	</g>
+</svg>
--- src/routes/creature-data/+page.svelte
+++ src/routes/creature-data/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { creature } from '@src/stores';
+  import TempCreature from '@src/components/creatures/TempCreature.svelte';
 </script>
 
 <main>
 	<h1>{$creature.name}</h1>
 	<p>Type: {$creature.type}</p>
 	<p>{$creature.description}</p>
+
+  <TempCreature />
 </main>
