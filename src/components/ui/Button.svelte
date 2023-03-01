8d60f1b55434cc73685d67e861b4807b231ec455
nbur4556 <nbur4556@gmail.com>
2023-02-28 21:46:28 -0600

    fix button formatting

--- src/components/ui/Button.svelte
+++ src/components/ui/Button.svelte
@@ -3,6 +3,6 @@
 	export let exClass = '';
 </script>
 
-<button on:click={onClick} class={`rounded-lg border-2 py-2 px-8 hover:bg-gray-200 ${exClass}`}
-	><slot /></button
->
+<button on:click={onClick} class={`rounded-lg border-2 py-2 px-8 hover:bg-gray-200 ${exClass}`}>
+	<slot />
+</button>
