77cc2c33cdabf2455a9dd1adfea2913d86e967ef
nbur4556 <nbur4556@gmail.com>
2023-02-28 18:13:02 -0600

    initial creature data page layout

--- src/routes/creature-data/+page.svelte
+++ src/routes/creature-data/+page.svelte
@@ -3,10 +3,16 @@
 	import TempCreature from '@src/components/creatures/TempCreature.svelte';
 </script>
 
-<main>
-	<h1>{$creature.name}</h1>
-	<p>Type: {$creature.type}</p>
-	<p>{$creature.description}</p>
-
-	<TempCreature scale={0.15}/>
+<main class="flex justify-center">
+	<section class="grid grid-cols-2 gap-2 m-10 w-8/12">
+	  <div class="flex col-span-2 justify-center">
+	    <div class="w-32">
+        <TempCreature />
+      </div>
+	  </div>
+  
+  	<h1 class="text-center">Name: {$creature.name}</h1>
+    <p class="text-center">Type: {$creature.type}</p>
+  	<p class="text-center col-span-2">{$creature.description}</p>
+	</section>
 </main>
