4fa61a99edf8df533b35931b0390c8cb0297b866
nbur4556 <nbur4556@gmail.com>
2023-03-03 16:10:46 -0600

    create initial account schema

---
+++ prisma/migrations/20230303220942_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
